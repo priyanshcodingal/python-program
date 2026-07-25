371998c8fba2545bf64d080582672d5645a2331d
priyanshcodingal <poojafr.singh20@gmail.com>
2026-07-25 11:36:41 +0200

    typecasting

---
+++ afterwork2.py/typecasting.py
@@ -0,0 +1,19 @@
+num=10
+print("num",num)
+print("type of num is:",type(num))
+name="prinyansh"
+print("name",name)
+print("type of name is:",type(name))
+height=1.49
+print("height",height)
+print("type of height is:",type(height))
+is_student=True
+print("type of is_student is:",type(is_student))
+
+print("**********type casting**********")
+num=str(num)
+print("type of num is:",type(num))
+
+height=int(height)
+print("height",height)
+print("type of height is:",type(height))
