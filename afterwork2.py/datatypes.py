985e2323bd58c13e357a4a9197d9ccc211525fbf
priyanshcodingal <poojafr.singh20@gmail.com>
2026-07-25 11:24:52 +0200

    datatypes

---
+++ afterwork2.py/datatypes.py
@@ -0,0 +1,8 @@
+num=10
+print("type of num is:",type(num))
+name="prinyansh"
+print("type of name is:",type(name))
+height=1.49
+print("type of height is:",type(height))
+is_student=True
+print("type of is_student is:",type(is_student))
