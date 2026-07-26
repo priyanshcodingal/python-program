1c4fbc0a90ed79d3dca911cd621ee42f97cfaa79
priyanshcodingal <poojafr.singh20@gmail.com>
2026-07-26 11:53:34 +0200

    percentage

---
+++ percentage.py
@@ -0,0 +1,8 @@
+maths=40
+science=70
+hindi=50
+english=60
+total=maths+science+hindi+english
+print("total percentege out of 100 is:",total)
+percentage=(total/400)*100
+print("percentege=",percentage)
