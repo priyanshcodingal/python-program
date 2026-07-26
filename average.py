7befd3271c763bea5caa39ad02da1510b57a0d98
priyanshcodingal <poojafr.singh20@gmail.com>
2026-07-26 11:20:36 +0200

    average

---
+++ average.py
@@ -0,0 +1,10 @@
+tree1=98
+tree2=94
+tree3=41
+tree4=96
+tree5=11
+total=tree1+tree2+tree3+tree4+tree5
+print("the total height of the trees is ",total)
+
+average=total/5
+print("the average height of the trees is",average)
