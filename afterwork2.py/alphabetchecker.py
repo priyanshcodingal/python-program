45382a8861fd4604acfa8068d7e90793d0b7aea2
priyanshcodingal <poojafr.singh20@gmail.com>
2026-07-29 20:14:04 +0200

    alphabetafterwork

---
+++ afterwork2.py/alphabetchecker.py
@@ -0,0 +1,6 @@
+a = C 
+user_input = input("Enter a letter: ")
+if user_input == "A" or user_input == "a" or user_input == "B" or user_input == "b" or user_input == "C" or user_input == "c" or user_input == "D" or user_input == "d" or user_input == "E" or user_input == "e" or user_input == "F" or user_input == "f" or user_input == "G" or user_input == "g" or user_input == "H" or user_input == "h" or user_input == "I" or user_input == "i" or user_input == "J" or user_input == "j" or user_input == "K" or user_input == "k" or user_input == "L" or user_input == "l" or user_input == "M" or user_input == "m" or user_input == "N" or user_input == "n" or user_input == "O" or user_input == "o" or user_input == "P" or user_input == "p" or user_input == "Q" or user_input == "q" or user_input == "R" or user_input == "r" or user_input == "S" or user_input == "s" or user_input == "T" or user_input == "t":
+    print("The letter is in the alphabet")
+else:
+    print("The letter is not in the alphabet")
