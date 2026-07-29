1e0f5d62d9cedf3e877915958626a442b5465447
priyanshcodingal <poojafr.singh20@gmail.com>
2026-07-29 19:54:09 +0200

    booleanproject

---
+++ myowncodetryfolder/boolean.py
@@ -0,0 +1,18 @@
+a = 10 
+b = -30 
+c = 0
+
+
+if a and b and c:
+    print("all numbers are true")
+
+else:
+    print("at least one number is false")
+
+
+if a > 0 or b > 0 or c > 0:
+    print("at least one number is greater than 0")
+else:
+    print("none of the value is greater than zero")
+
+
---
+++ myowncodetryfolder/greater.py
@@ -0,0 +1,5 @@
+num=int(input("enter"))
+if num>0:
+    print(num,"is greater then 0")
+else:
+    print(num,"is less then 0")
