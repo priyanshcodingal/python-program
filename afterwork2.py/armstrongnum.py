8b5e800e9f712b142df69d10f133547199fcaeae
priyanshcodingal <poojafr.singh20@gmail.com>
2026-08-08 11:34:01 +0200

    armstrongproject

---
+++ afterwork2.py/armstrongnum.py
@@ -0,0 +1,15 @@
+num = int(input("enter a number:"))
+temp = num
+sum = 0
+
+while temp > 0:
+    digit = temp % 10
+    sum = sum + (digit**3)
+    temp = temp//10
+
+if sum == num:
+    print("armstrong number")
+else:
+    print("not armstrong number")
+
+
