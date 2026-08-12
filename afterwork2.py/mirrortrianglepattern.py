6d1db4e7104135be1c5194cb229af3d1cc49fd7e
priyanshcodingal <poojafr.singh20@gmail.com>
2026-08-12 20:23:57 +0200

    mirrortrianglework

---
+++ afterwork2.py/mirrortrianglepattern.py
@@ -0,0 +1,8 @@
+rows=int(input("enter a number of rows:"))
+for i in range(1, rows+1):
+    for j in range(1, i+1):
+        print("*", end="")
+for i in range(1, i+5):
+    for j in range(1, i+5):
+        print("*", end="")
+    print()
