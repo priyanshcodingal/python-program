604182b8fdf3b0f3785512a7a5fed8d012c09052
priyanshcodingal <poojafr.singh20@gmail.com>
2026-08-22 11:26:23 +0200

    polygonptojectt

---
+++ afterwork2.py/polygon.py
@@ -0,0 +1,13 @@
+import turtle
+turtle.Screen().bgcolor("red")
+turtle.Screen().setup(300,400)
+
+polygon = turtle.Turtle()
+num_sides=6
+side_lenght=70
+angle=360.0/num_sides
+
+for i in range(num_sides):
+    polygon.forward(side_lenght)
+    polygon.right(angle)
+turtle.done()
