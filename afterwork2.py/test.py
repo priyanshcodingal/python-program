b338888b38ddebd6816efd9c1507c148c9b3244b
priyanshcodingal <poojafr.singh20@gmail.com>
2026-08-24 19:30:35 +0200

    functionproject

--- afterwork2.py/test.py
+++ afterwork2.py/test.py
@@ -1,48 +1,13 @@
 import turtle
-turtle.Screen().bgcolor("blue")
+
+turtle.Screen().bgcolor("red")
 turtle.Screen().setup(300,400)
 
 board=turtle.Turtle()
 
-
-
-for i in range(4):
-    board.left(90)
-    board.forward(100)
-for i in range(2):
-    board.left(45)
-    board.forward(70)
-
+board.forward(100)
+board.left(45)
+board.right(90)
+board.forward(70)
 
 turtle.done()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ function.py
@@ -0,0 +1,6 @@
+def well_wishes():
+    print("hello")
+    print("are you doing well at school today?")
+
+well_wishes()
+
