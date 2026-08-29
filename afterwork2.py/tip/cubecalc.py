ffdd39f6ed4b47d71e801dcf02ce845fa004bb10
priyanshcodingal <poojafr.singh20@gmail.com>
2026-08-29 11:34:16 +0200

    cubecalcproject

---
+++ afterwork2.py/tip/cubecalc.py
@@ -0,0 +1,12 @@
+def cube(num):
+    return num * num * num
+
+def by_three(num):
+    if num % 3==0:
+        return cube(num)
+    else:
+        return False
+
+
+print(by_three(9))
+print(by_three(4))
