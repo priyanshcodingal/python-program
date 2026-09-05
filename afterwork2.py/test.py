6c55fc0a922dcb7051316abfc2e5c2af3185599b
priyanshcodingal <poojafr.singh20@gmail.com>
2026-09-05 11:26:07 +0200

    datetimemoduleproject

--- afterwork2.py/test.py
+++ afterwork2.py/test.py
@@ -1,13 +1,3 @@
-import turtle
 
-turtle.Screen().bgcolor("red")
-turtle.Screen().setup(300,400)
 
-board=turtle.Turtle()
 
-board.forward(100)
-board.left(45)
-board.right(90)
-board.forward(70)
-
-turtle.done()
