78ddea0e2c0f26ffcc0a856f94e03367751c8cbd
priyanshcodingal <poojafr.singh20@gmail.com>
2026-09-16 20:12:48 +0200

    contrycodeproject

---
+++ afterwork2.py/contrycode.py
@@ -0,0 +1,5 @@
+contrycode = {"india":"0021", "Australia":"0025", "Nepal":"00977"}
+
+print("contry code for India : ",contrycode.get("india","notfound"))
+print("contry code for japan : ",contrycode.get("japan","notfound"))
+print("contry code for Nepal : ",contrycode.get("Nepal","notfound"))
